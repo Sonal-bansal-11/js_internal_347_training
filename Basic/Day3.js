0fce21c773df1f0948f1d89547710664bd9b7204
Sonal Bansal <157891272+Sonal-bansal-11@users.noreply.github.com>
2025-12-10 05:31:13 +0000

    Day 3

---
+++ Basic/Day3.js
@@ -0,0 +1,60 @@
+// let n;
+// function fun(n){
+//   return n*2;
+// }
+// let ch=fun(72);
+// console.log(ch);
+
+// let myarr=[1,2,3,4,5,"apple","banana",ch]
+// console.log(myarr);
+
+// let nums=[10,20,30]
+
+// nums.push(40);
+// nums.pop();
+// nums.unshift(5);
+// nums.shift();
+// nums.include(20);
+// nums.indexOf(30);
+
+// let fruits=["Apple","Banana","Guava","Orange","Grapes"];
+// console.log(fruits);
+// for (let value of fruits)
+//   console.log(value);
+// fruits.push("Papaya");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.unshift("Kiwi");
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// console.log(fruits.includes("Mango"));
+// console.log(fruits.indexOf("Banana"));
+
+// let numbers=[1,2,3];
+// let doubled=numbers.map(n => n*2);
+// console.log(doubled);
+
+// let nums=[10,25,30,40];
+// let result=nums.filter(n=>n>20);
+// console.log(result);
+
+// let total=nums.reduce((acc,val)=>acc+val,0);
+
+// //create an array of numbers [2,4,6,8] and return a new array with each value doubled using map()
+// let array=[2,4,6,8];
+// let doubled=array.map(n=>n*2);
+// console.log(doubled);
+
+// //From numbers [10,25,30,5,60] filter values greater than 20
+// let numbers=[10,25,30,5,60];
+// let result=numbers.filter(n=>n>20);
+// console.log(result);
+
+// //Calculate sum using reduce
+// let nums=[1,2,3,4,5];
+// let sum=nums.reduce((acc,val)=>acc+val,0);
+// console.log(sum);
+
+
